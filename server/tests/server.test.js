2977783ff4ee281356c0b22c092482c5cabaf5b8
Sergei Sorokin <serebanyshka7@gmail.com>
2018-07-31 00:49:54 +0300

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,22 @@ const { User } = require('./../modules/User');
 const { Todo } = require('./../modules/Todo');
 const { app } = require('./../server');
 
+const todos = [{text: 'First test todo'}, {text: 'Second test todo'}];
+
 beforeEach(done => {
-  Todo.remove({}).then( () => done());
+  Todo.remove({}).then(() => Todo.insertMany(todos)).then(() => done());
+});
+
+describe('GET /todos', () => {
+  it('should return all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done);
+  });
 });
 
 describe('POST /todos', () => {
@@ -22,7 +36,7 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo
-          .find()
+          .find({text})
           .then( todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -44,7 +58,7 @@ describe('POST /todos', () => {
         Todo
           .find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(err => done(err))
