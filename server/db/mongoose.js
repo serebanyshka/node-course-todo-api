afb7db04f0390e35cfa376865b8ea94810d84f6c
Sergei Sorokin <serebanyshka7@gmail.com>
2018-07-31 17:14:37 +0300

    Change uri For heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const url = "mongodb://user:todo2018@ds261521.mlab.com:61521/todoapp"
+//process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(url, {useNewUrlParser: true});
+
+const connect = (name. psw) => {
+  mongoose.connect(url, {useNewUrlParser: true});
+}
 
 module.exports = { mongoose };
