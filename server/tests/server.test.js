f665df4d82bd48a6ebcdb7bbc6c5aae3f17abefe
Sergei Sorokin <serebanyshka7@gmail.com>
2018-07-31 16:50:32 +0300

    Corrected mistakes with user.js and todo.js

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,8 +1,8 @@
 const expect = require('expect');
 const request = require('supertest');
 const { ObjectID } = require('mongodb');
-const { User } = require('./../models/User');
-const { Todo } = require('./../models/Todo');
+const { User } = require('./../models/user');
+const { Todo } = require('./../models/todo');
 const { app } = require('./../server');
 
 
