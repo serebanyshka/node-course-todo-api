ae9b602148c8a7202715708da941f7ee291e3b03
Sergei Sorokin <serebanyshka7@gmail.com>
2018-07-31 12:56:25 +0300

    Add queries playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,8 +1,16 @@
+const { ObjectID } = require('mongodb');
 const { mongoose } = require('./../server/db/mongoose');
 const { Todo } = require('./../server/modules/todo');
 
 const strID = "5b5f8d21ef47f72470c64f52";
-
-Todo.find({ _id: strID }).then(todos => console.log('Todos '+ todos));
-Todo.findOne({ _id: strID}).then(todo => console.log('Todo' + todo));
-Todo.findById({ _id: strID}).then(todo => console.log('by id Todo ' + todo));
+if(!ObjectID.isValid(strID)) {
+  return console.log("Id isn't valid");
+}
+// Todo.find({ _id: strID }).then(todos => console.log('Todos '+ todos));
+// Todo.findOne({ _id: strID}).then(todo => console.log('Todo' + todo));
+Todo.findById({ _id: strID}).then(todo => {
+  if(!todo) {
+    // TODO: todo not found
+    console.log('Object with this id not found');
+  }
+  console.log('by id Todo ' + todo);}).catch(err => console.log(err));
