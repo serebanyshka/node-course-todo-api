c411afa040a966219cd7f156eff6c7c3469176ce
Sergei Sorokin <serebanyshka7@gmail.com>
2018-08-01 03:05:39 +0300

    Change process.env.uri for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,9 @@ const env = process.env.NODE_ENV || 'development';
 
 if(env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://user:todo2018@ds261521.mlab.com:61521/todoapp'; // 'mongodb://localhost:27017/TodoApp';
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  process.env.HEROKU_POSTGRESQL = 'mongodb://user:todo2018@ds261521.mlab.com:61521/todoapp';
+
 } else if(env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI;
-
+const uri = process.env.MONGODB_URI || process.env.HEROKU_POSTGRESQL;
 mongoose.Promise = global.Promise;
 console.log(uri);
-mongoose.connect(uri, {useNewUrlParser: true});
+if(!uri) {
+  uri = 'mongodb://user:todo2018@ds261521.mlab.com:61521/todoapp';
+}
+console.log(uri);
+mongoose.connect( (uri, {useNewUrlParser: true});
 
 
 module.exports = { mongoose };
