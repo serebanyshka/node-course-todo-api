940a2144af7ba5a13409d3662433323795bbebb6
Sergei Sorokin <serebanyshka7@gmail.com>
2018-08-01 03:07:43 +0300

    Change process.env.uri for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,7 +7,7 @@ if(!uri) {
   uri = 'mongodb://user:todo2018@ds261521.mlab.com:61521/todoapp';
 }
 console.log(uri);
-mongoose.connect( (uri, {useNewUrlParser: true});
+mongoose.connect(uri, {useNewUrlParser: true});
 
 
 module.exports = { mongoose };
