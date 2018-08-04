4e818ab161657e1d6e791afeb9fc17f8da1c3b49
Sergei Sorokin <serebanyshka7@gmail.com>
2018-08-04 16:53:27 +0300

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -258,3 +258,24 @@ describe('POST /users/login', () => {
         });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove of token on logout', done => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        User
+          .findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+});
